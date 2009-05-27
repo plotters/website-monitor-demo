032420efdd99c3717909714ebd33c4fc742240fb
dfrolov <dfrolov@8c02d802-49f9-11de-a65c-33beeba39556>
2009-05-27 12:07:00 +0000

    Added PageWrapper

---
+++ trunk/SiteMonitor/Sources/ru/demax/sitemonitor/components/PageWrapper.java
@@ -0,0 +1,11 @@
+package ru.demax.sitemonitor.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.components.ERDCustomComponent;
+
+public class PageWrapper extends ERDCustomComponent {
+    public PageWrapper(WOContext context) {
+        super(context);
+    }
+}
