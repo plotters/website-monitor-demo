0c6f0273f6305bc5636c69a009f62f539461501e
dfrolov <dfrolov@8c02d802-49f9-11de-a65c-33beeba39556>
2009-05-27 14:40:29 +0000

    Initializing handler upon session creation

--- SiteMonitor/Sources/ru/demax/sitemonitor/Session.java
+++ SiteMonitor/Sources/ru/demax/sitemonitor/Session.java
@@ -5,6 +5,8 @@ import er.extensions.appserver.ERXSession;
 public class Session extends ERXSession {
 	private static final long serialVersionUID = 1L;
 
+	public Handler handler = new Handler();
+	
 	public Session() {
 	}
 }
